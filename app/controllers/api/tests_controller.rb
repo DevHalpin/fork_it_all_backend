7e6c767d5a047b077b902c51a24b920c5e0f30df
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-23 15:42:05 -0700

    have got the name for penne out

--- app/controllers/api/tests_controller.rb
+++ app/controllers/api/tests_controller.rb
@@ -4,10 +4,18 @@ class Api::TestsController < ApplicationController
   def index
     response = RestClient.get 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
 
+    # newResponse = JSON.parse(response)
     puts response
+    parsed = JSON.parse(response.body)
+    puts parsed.class
+    mealsObj = parsed['meals']
+    puts mealsObj.first['strMeal']
+    puts mealsObj.each
+    # strMeal = newResponse[:strMeal]
+    
 
     render :json => {
-      message: response
+      message: mealsObj.first['strMeal']
     }
   end
 end
