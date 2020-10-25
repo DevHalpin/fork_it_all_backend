8766f3496405efc4899019c4b881d56197288cb0
DevHalpin <echidnablade@hotmail.com>
2020-10-24 17:10:25 -0700

    add ability to add records to db based on user searches if there are not in db already

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,85 +1,61 @@
-# class Meal
-#   attr_accessor :name, :meal_id, :ingredient, :instructions, :ingredients, :measures
-
-#   @@all = []
-
-#   def initialize(name:, meal_id:, ingredient:)
-#     @name = name
-#     @meal_id = meal_id
-#     @ingredient = ingredient
-#     @ingredients = []  ## will be strings
-#     @measures = []  ## will be strings
-#     @@all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-
-
-# end
-
-# class Ingredient
-#   attr_accessor :name, :meals
-
-#   @@all = []
-
-#   def initialize(name)
-#     @name = name
-#     @meals = []  ### will contain OBJECTS
-#     @@all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-#   def self.find_by_ingredient(ingredient)
-#     self.all.find {|ing_obj| ing_obj.name == ingredient}
-#   end
-
-# end
-
 class Api::RecipesController < ApplicationController
   require 'rest-client'
   require 'json'
 
-  ## resposible for making drinks.
-
-  # def self.get_meals(ingredient)
-  #   response = RestClient.get 'https://www.themealdb.com/api/json/v1/1/filter.php?i=#{ingredient}'
-
-  #   meals = JSON.parse(response)['meals']
-  #   new_ingredient = Ingredient.new(ingredient)
-  #   meals.each do |meal_details|
-  #     name = meal_details["strMeal"]
-  #     meal_id = meal_details["idMeal"]
-  #     new_meal = Meal.new(name: name, meal_id: meal_id, ingredient: ingredient)
-  #     new_ingredient.meals << new_meal
-  #   end
-  # end
-
-
-  def index ## meal_object
+  def get_recipe(word)
+    check = Recipe.where(Recipe.arel_table[:name].matches("%#{word}%"))
     ## what is my endpoint?
-    response = RestClient.get 'https://www.themealdb.com/api/json/v1/1/random.php'
-    # how do I parse the response into meaningful json data?
-    meal_details = JSON.parse(response)["meals"].first
+    if ( check.empty? )
+      response = RestClient.get("https://www.themealdb.com/api/json/v1/1/search.php?s=#{word}")
+      meal = JSON.parse(response)
 
-    ## what do I want to do from there?
-    ## add an attribute on my EXISTING meal obj
-    # meal.instructions = meal_details["strInstructions"]
-    # meal_details.keys.each do |k|
+      if (!meal['meals'].nil?)
+        # how do I parse the response into meaningful json data?
+        meal_details = meal["meals"].first
 
-    #   meal.ingredients << meal_details[k] if (k.include?("Ingredient")) && meal_details[k] 
-    #   meal.measures << meal_details[k] if (k.include?("Measure")) && meal_details[k]
-    # end
-    message = Array.new
-    message.push(meal_details)
+        Recipe.create!(
+          :id => meal_details['idMeal'],
+          :name => meal_details['strMeal'],
+          :region => meal_details['strArea'],
+          :instructions => meal_details['strInstructions'],
+          :meal_image => meal_details['strMealThumb'],
+          :meal_type => meal_details['strTags'],
+          :video_url => meal_details['strYoutube'] 
+        )
+
+        # go through each key in meal_details
+        meal_details.keys.each do | key |
+        # if key has word ingredient and is not null and is not empty string
+          if (key.include?("Ingredient") && !meal_details[key].empty?)
+            #  check if in ingredients db
+            if (Ingredient.where(name: meal_details[key]).empty?)
+        # if exists
+        # check measures table for measures
+        # if not, push to measures table
+              Ingredient.create!(
+                :name => meal_details[key]
+              )
+        #  if not, push to db and push to measures
+            else
+              puts 'Here'
+            end
+          end
+        end
+
+        meal_details
+      else
+        'Nothing found'
+      end
+    else
+      check
+    end
+  end
+
+  def index
+    recipe = get_recipe('Shepard')
 
     render :json => {
-      message: meal_details.to_json
+      message: recipe.to_json
     }
   end
 
