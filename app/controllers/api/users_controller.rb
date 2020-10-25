4528f47f7f615a032f467ef47b081cf45c2e0c19
DevHalpin <echidnablade@hotmail.com>
2020-10-24 17:08:22 -0700

    clean up unused code

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,15 +4,6 @@ class Api::UsersController < ApplicationController
 
 
     @users = User.all.order(created_at: :desc)
-    # puts @users.inspect
-    
-
-    @users.each do | user |
-      example = Array(user)
-      puts example.inspect
-    end
-
-    # puts users.inspect
 
     render :json => {
       message: @users.to_json
