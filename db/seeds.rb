ce8cf83bed6ecc849c717ce009740ba7760e5635
DevHalpin <echidnablade@hotmail.com>
2020-10-25 14:19:59 -0700

    Add recipe to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,9 @@ end
   User.create! email: email, profile_picture: profile_picture, handle: username
 end
 
+## RECIPE
+Recipe.create! name: "Spicy Arrabiata Penne", region: "Italian", instructions: "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes. In a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil. Drain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.", meal_image: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg", meal_type: "Pasta,Curry", video_url: "https://www.youtube.com/watch?v=1IszT_guI08"
+
 ## TWISTS
 100.times do
   recipe_id = 1
