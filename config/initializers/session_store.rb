f7472f8138618097340d79cc55691c4c7c9476bd
DevHalpin <echidnablade@hotmail.com>
2020-11-01 19:39:18 -0800

    Add domain on session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_forkitall_session', domain: "https://stark-shelf-20245.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: '_forkitall_session', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_forkitall_session'
 end
