271e962b81d78537234a0eaba86c2d14b087ad0d
DevHalpin <echidnablade@hotmail.com>
2020-11-01 19:58:17 -0800

    Set session cookie to none

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_forkitall_session', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_forkitall_session', domain: :all, same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_forkitall_session'
 end
