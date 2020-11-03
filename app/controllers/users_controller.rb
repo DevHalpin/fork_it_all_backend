99a689329788b9ca1661234aca951c58afbcc724
DevHalpin <echidnablade@hotmail.com>
2020-11-02 16:03:33 -0800

    Heroku 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def myTwists
     puts @current_user
-    @myTwists = Recipe.joins("LEFT JOIN twists ON twists.recipe_id = recipes.id AND twists.user_id = ?", @current_user.id)
+    @myTwists = Recipe.joins("LEFT JOIN twists ON twists.recipe_id = recipes.id AND twists.user_id = ?", session[:user_id])
     puts @myTwists.inspect
     # Client.where("orders_count = ?", params[:orders])
     # @myTwists = Recipe.joins(:twists).where(user_id: 1).select("recipes.id as recipe_id, recipes.name, recipes.meal_image, twists.id as twist_id, twists.content")
