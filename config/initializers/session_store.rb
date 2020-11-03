673632e079737d9db8697f88223bfd9623948f2c
DevHalpin <echidnablade@hotmail.com>
2020-11-02 17:35:27 -0800

    Heroku 11

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: '_forkitall_session', domain: :all, same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_forkitall_session'
 else
   Rails.application.config.session_store :cookie_store, key: '_forkitall_session'
 end
